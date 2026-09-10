a8d6b13efa6be47c9eff5c0936b0aeb29ac89f91
t0o8 <123jasonhun@gmail.com>
2026-09-10 00:20:12 -0400

    added a mouse pull effect when mouse is held

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@ pub const PARTICLES_START_POS_X: f32 = 200.0;
 pub const PARTICLES_START_POS_Y: f32 = 200.0;
 
 pub const PARTICLE_SIZE:f32 = 4.0;
-pub const PARTICLE_COUNT:u32 = 1000;
+pub const PARTICLE_COUNT:u32 = 700;
 
 pub const GRAVITY: f32 = -19.8;
 
@@ -10,7 +10,7 @@ pub const DAMPENING:f32 = 0.2;
 pub const SMOOTHING_RADIUS: f32 = 2.5;
 pub const TARGET_DENSITY: f32 = 1.0;
 pub const PRESSURE_MULTIPLYER: f32 = 2000.0;
-pub const VISCOSITY: f32 = 13.0;
+pub const VISCOSITY: f32 = 25.0;
 
 pub const SCREEN_START_X:f32 = 100.0; // given in pixels
 pub const SCREEN_START_Y:f32 = 100.0; // given in pixels
@@ -22,4 +22,5 @@ pub const X_LEFT_BOUNDARY: f32 = 0.0;
 pub const Y_BOTTOM_BOUNDARY: f32 = 0.0;
 pub const Y_TOP_BOUNDARY: f32 = 85.0;
 
-pub const MOUSE_EFFECT_RADIUS: f32 = 25.0;
+pub const MOUSE_EFFECT_RADIUS: f32 = 10.0;
+pub const MOSUE_PULL_STRENGTH: f32 = 300.0;
